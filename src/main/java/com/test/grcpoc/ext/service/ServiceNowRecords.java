a51c593284ab45ec3b9b94b1a7a435eb151ad10f
sameerdiwse <sameerdiwse9090@gmail.com>
2023-03-27 11:23:58 +0530

    made changes in order

--- src/main/java/com/test/grcpoc/ext/service/ServiceNowRecords.java
+++ src/main/java/com/test/grcpoc/ext/service/ServiceNowRecords.java
@@ -16,27 +16,26 @@ public class ServiceNowRecords implements IServiceNowRecords {
 
 	@Override
 	public PostEntity extractData(ResponseEntity<ServiceNowResource> response) {
-		ObjectMapper mapper = new ObjectMapper();  
 		
-		ArrayList<field> fieldAl = new ArrayList<>();
 		field f = new field();
 		
-		f.setDataType("STRING_TYPE");
 		f.setName("OPSS-Emp:First Name");
 		f.setValue(response.getBody().getResult().get(0).getDisplay_name());
+		f.setDataType("STRING_TYPE");
 		
 		field f0 = new field();
 		
-		f0.setDataType("STRING_TYPE");
 		f0.setName("OPSS-Emp:Last Name");
 		f0.setValue(response.getBody().getResult().get(0).getSys_class_name());
+		f0.setDataType("STRING_TYPE");
 		
 		field f1 = new field();
 		
-		f1.setDataType("STRING_TYPE");
 		f1.setName("OPSS-Emp:Email");
 		f1.setValue("sameer@gmail.com");
+		f1.setDataType("STRING_TYPE");
 		
+		ArrayList<field> fieldAl = new ArrayList<>();
 		fieldAl.add(f);
 		fieldAl.add(f0);
 		fieldAl.add(f1);

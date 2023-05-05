6436cccec14440b0e02d51a8a07042005d84c06a
sameerdiwse <sameerdiwse9090@gmail.com>
2023-05-05 08:59:13 +0530

    added JSPs

--- src/main/java/com/test/grcpoc/ext/entity/PostEntity.java
+++ src/main/java/com/test/grcpoc/ext/entity/PostEntity.java
@@ -10,34 +10,32 @@ public class PostEntity {
 	 private String name;
 	 private fields fields;
 	 
-public String getTypeDefinitionId() {
-	return typeDefinitionId;
-}
-public void setTypeDefinitionId(String typeDefinitionId) {
-	this.typeDefinitionId = typeDefinitionId;
-}
-public String getPrimaryParentId() {
-	return primaryParentId;
-}
-public void setPrimaryParentId(String primaryParentId) {
-	this.primaryParentId = primaryParentId;
-}
-
-
-  public String getName() { return name; } public void setName(String name) {
-  this.name = name; }
- 
-public fields getFields() {
-	return fields;
-}
-public void setFields(fields fields) {
-	this.fields = fields;
-}
-
-
-
-
-
-
+	public String getTypeDefinitionId() {
+		return typeDefinitionId;
+	}
+	public void setTypeDefinitionId(String typeDefinitionId) {
+		this.typeDefinitionId = typeDefinitionId;
+	}
+	public String getPrimaryParentId() {
+		return primaryParentId;
+	}
+	public void setPrimaryParentId(String primaryParentId) {
+		this.primaryParentId = primaryParentId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public fields getFields() {
+		return fields;
+	}
+	public void setFields(fields fields) {
+		this.fields = fields;
+	}
+	 
+	 
+	 
 
 }
